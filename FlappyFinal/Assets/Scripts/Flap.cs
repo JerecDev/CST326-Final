9afe98242aba37f8d458e542c3a00f9f1415a50a
JerecDev <zachabbett@gmail.com>
2020-04-22 15:32:56 -0700

    1st
    1stPush

---
+++ FlappyFinal/Assets/Scripts/Flap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Rigidbody bird;
+    void Start()
+    {
+        bird = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
